f4b111a9798685547f84414bdc21aebc7ef120f2
DESKTOP-1E9IN30\82104 <tpoluol3@gmail.com>
2023-03-22 00:21:35 +0900

    :wrench: Setup

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Outfit: ['Outfit'],
+      },
+      colors: {
+        White: 'hsl(0, 0%, 100%)',
+        LigthGray: 'hsl(212, 45%, 89%)',
+        GrayishBlue: 'hsl(220, 15%, 55%)',
+        DarkBlue: 'hsl(218, 44%, 22%)',
+      },
+    },
   },
   plugins: [],
 };
